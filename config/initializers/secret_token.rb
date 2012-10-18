de1d8d414950fc634bb787641c61bbd18b3deb08
vredniy <dnd.pliz@gmail.com>
2012-10-18 15:13:17 +0400

    lets play

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BlogMe::Application.config.secret_token = 'b90551f384888fd86ece82ace5023b8c4b8ddc0cb878ebc9a2190f412347156e5a1c6d8b71bb4dd1a1d717a36be82934e2e64a65f56141635cfb4d97b1aa40c6'
