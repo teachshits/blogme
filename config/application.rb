b17cfc98c0f6fb74687372f676acb3cd60ea3d13
vredniy <dnd.pliz@gmail.com>
2012-10-18 15:19:19 +0400

    config tuning

--- config/application.rb
+++ config/application.rb
@@ -64,5 +64,11 @@ module BlogMe
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    config.generators do |g|
+      g.stylesheets false
+      g.helper false
+      g.javascripts false
+    end
   end
 end
